0484d80c44b975ec01189e72b55d91de06137be3
Mohamed Takrayout <mohtakra@e1r9p3.1337.ma>
2024-04-01 00:08:03 +0000

    last update for script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,12 +4,12 @@ sed -i 's/bind-address = 127.0.0.1/bind-address=0.0.0.0/g' /etc/mysql/mariadb.co
 service mysql start
 mysql -u root <<EOF 2> /dev/null
     CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-    CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY $MYSQL_PASSWORD;
+    CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
     GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%';
     FLUSH PRIVILEGES;
 EOF
 
-mysql -u root -e "ALTER USER 'root'@'localhost' identified by $MYSQL_ROOTPASSWORD;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';"
 
 mysqladmin -u root -p$MYSQL_ROOTPASSWORD shutdown
 
